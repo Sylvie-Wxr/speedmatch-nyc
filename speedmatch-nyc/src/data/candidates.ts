ce57a4340ae8ef0d42ddf521c71d0dc9a3264415
Sylvie-Wxr <sylvie_wu125@outlook.com>
2025-09-18 16:26:44 -0700

    change key photo

--- speedmatch-nyc/src/data/candidates.ts
+++ speedmatch-nyc/src/data/candidates.ts
@@ -17,32 +17,18 @@ export const candidates: Candidate[] = [
     party: "Independent",
     altText: "Black and white portrait of Andrew Cuomo in a suit, hands together, serious expression."
   },
+
   {
     id: 3,
-    name: "Jean Anglade",
-    image: "/images/candidates/jean-anglade.webp",
-    website: "https://www.anglade4mayorny25.com/",
-    party: "Independent",
-    altText: "Black and white portrait of Jean Anglade wearing a checkered shirt, slightly smiling."
-  },
-  {
-    id: 4,
     name: "Irene Estrada", 
     image: "/images/candidates/irene-estrada.webp",
     website: "https://www.facebook.com/groups/ireneestrada/about",
     party: "Conservative Party",
     altText: "Black and white portrait of Irene Estrada smiling with long curly hair."
   },
+
     {
-    id: 5,
-    name: "Kyle Gutierrez",
-    image: "/images/candidates/kyle-gutierrez.webp", 
-    website: "https://kyle4mayor.nyc/",
-    party: "Independent",
-    altText: "Black and white portrait of Kyle Gutierrez with long curly hair, mustache, and suit."
-  },
-    {
-    id: 6,
+    id: 4,
     name: "Joseph Hernandez",
     image: "/images/candidates/joseph-hernandez.webp", 
     website: "https://www.hernandezfornyc.com/",
@@ -50,15 +36,7 @@ export const candidates: Candidate[] = [
     altText: "Black and white portrait of Joseph Hernandez smiling with a hat and suit."
   },
   {
-    id: 7,
-    name: "Abby Laurel-Smith",
-    image: "/images/candidates/abby-laurel-smith.webp",
-    website: "https://www.rebuildnewyork.city/",
-    party: "Independent",
-    altText: "Black and white portrait of Abbey Laurel-Smith wearing a suit and tie, neutral expression."
-  },
-  {
-    id: 8,
+    id: 5,
     name: "Zohran Mamdani",
     image: "/images/candidates/zohran-mamdani.webp",
     website: "https://www.zohranfornyc.com/",
@@ -66,7 +44,7 @@ export const candidates: Candidate[] = [
     altText: "Black and white portrait of Zohran Mamdani with a beard, smiling slightly, wearing a light top."
   },
    {
-    id: 8,
+    id: 6,
     name: "Curtis Sliwa",
     image: "/images/candidates/curtis-sliwa.webp",
     website: "https://www.sliwafornyc.com/",
